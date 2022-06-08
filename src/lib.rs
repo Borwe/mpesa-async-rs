c225fd9b41475e445e7afe85ee6769bcd5ef46cf
brian.orwe <brian.orwe@gmail.com>
2022-06-08 13:17:52 +0300

    Add base64

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use reqwest::Client as WebClient;
+use reqwest::Method;
+
+const SANDBOX_URL: &str = "https://sandbox.safaricom.co.ke/";
+const MAIN_URL: &str = "https://safaricom.co.ke/";
+
+struct Client {
+    web_client: WebClient
+}
+
+impl Client {
+    /// Create a new client Object
+    async fn new(cons_key: String, cons_secret: String, test: bool) {
+        let client = Client{ web_client: WebClient::new() };
+        let req_builder = match test{
+            true => client.web_client.request(Method::GET, SANDBOX_URL),
+            false => client.web_client.request(Method::GET, MAIN_URL)
+        };
+
+        //(cons_key+String::from(":")+cons_secret).as_bytes().to_
     }
 }
--- tests/generate_token.rs
+++ tests/generate_token.rs
@@ -1,5 +1,9 @@
+use dotenv::dotenv;
 
 #[tokio::test]
 async fn test_generate_token(){
-    //let client = mpesa::Client::new()
+    dotenv().ok();
+    let cons_key = std::env::var("CONSUMER_KEY").unwrap();
+    let cons_secret = std::env::var("CONSUMER_SECRET").unwrap();
+    //let client = mpesa_async::Client::new(cons_key, cons_secret,true).await.unwrap();
 }
