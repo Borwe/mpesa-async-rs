ff1cd9aaeb7f9073bfb624c49b79ea187bede001
brian.orwe <brian.orwe@gmail.com>
2022-06-08 15:39:42 +0300

    Can now get access token

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,86 @@
 use reqwest::Client as WebClient;
 use reqwest::Method;
+use serde::{Deserialize, Serialize};
 
-const SANDBOX_URL: &str = "https://sandbox.safaricom.co.ke/";
-const MAIN_URL: &str = "https://safaricom.co.ke/";
+const SANDBOX_OAUTH_URL: &str = "https://sandbox.safaricom.co.ke/oauth/v1/generate";
+const MAIN_OAUTH_URL: &str = "https://safaricom.co.ke/oauth/v1/generate";
 
-struct Client {
-    web_client: WebClient
+pub struct Client {
+    web_client: WebClient,
+    pub req_secs: Option<u32>,
+    pub access_token: Option<String>
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct GeneratorReply{
+    expires_in: String,
+    access_token: String,
+}
+
+mod error{
+    use super::*;
+    #[derive(Deserialize, Serialize, Debug)]
+    pub struct ClientCreationError{
+        pub error_message: String,
+        pub request_id: String,
+        pub error_code: String
+    }
 }
 
 impl Client {
     /// Create a new client Object
-    async fn new(cons_key: String, cons_secret: String, test: bool) {
-        let client = Client{ web_client: WebClient::new() };
+    pub async fn new(cons_key: String, cons_secret: String, test: bool)
+        -> Result<Client, error::ClientCreationError>{
+        let mut client = Client{ 
+            web_client: WebClient::new(),
+            req_secs: None,
+            access_token: None
+        };
         let req_builder = match test{
-            true => client.web_client.request(Method::GET, SANDBOX_URL),
-            false => client.web_client.request(Method::GET, MAIN_URL)
+            true => client.web_client
+                .request(Method::GET, SANDBOX_OAUTH_URL),
+            false => client.web_client.request(Method::GET, MAIN_OAUTH_URL)
         };
 
-        //(cons_key+String::from(":")+cons_secret).as_bytes().to_
+        let mut to_encode = cons_secret;
+        to_encode.insert_str(0,":");
+        to_encode.insert_str(0,&cons_key);
+        let mut encoded: String = base64::encode(to_encode);
+        encoded.insert_str(0,"Basic ");
+        let request = req_builder.header("Authorization",encoded.into_bytes())
+            .header("Content-Type","application/json".as_bytes())
+            .query(&[("grant_type","client_credentials")])
+            .build().unwrap();
+
+        let response  = client.web_client.execute(request).await.unwrap();
+        if response.status().is_client_error(){
+            match response
+                .json::<std::collections::HashMap<String,String>>()
+                .await{
+                Ok(output) => Err(error::ClientCreationError{
+                            error_message: output
+                                .get("errorMessage").unwrap().clone(),
+                            request_id: output
+                                .get("requestId").unwrap().clone(),
+                            error_code: output
+                                .get("errorCode").unwrap().clone() 
+                        })
+                ,
+                Err(_) => Err(error::ClientCreationError{
+                              error_message: String::from("unkown"),
+                              request_id: String::from("unkown"),
+                              error_code: String::from("unkown") 
+                          })
+            }
+        }else{
+            let output = response
+                .json::<GeneratorReply>()
+                .await.unwrap();
+            println!("CLIENT SUCCESS: {:?}",output);
+            client.access_token = Some(output.access_token);
+            client.req_secs = Some(output
+                   .expires_in.parse::<u32>().unwrap());
+            Ok(client)
+        }
     }
 }
--- tests/generate_token.rs
+++ tests/generate_token.rs
@@ -5,5 +5,18 @@ async fn test_generate_token(){
     dotenv().ok();
     let cons_key = std::env::var("CONSUMER_KEY").unwrap();
     let cons_secret = std::env::var("CONSUMER_SECRET").unwrap();
-    //let client = mpesa_async::Client::new(cons_key, cons_secret,true).await.unwrap();
+
+    let fake_cons_key = std::env::var("FAKE_CONSUMER_KEY").unwrap();
+    let fake_cons_secret = std::env::var("FAKE_CONSUMER_SECRET").unwrap();
+
+    //success status if there is internet
+    let client = mpesa_async::Client::new(cons_key, cons_secret,true).await.unwrap();
+    assert!(client.req_secs.unwrap()>1);
+
+    //failure should return a ClientCreationError
+    match mpesa_async::Client::new(fake_cons_key,
+           fake_cons_secret,true).await{
+        Ok(_) => panic!("Should never reach here, as is supposed to fail"),
+        Err(err) => assert!(err.error_code.len()>0)
+    }
 }
